f5b7c9dfb0a6f9634174edfaee7d807c8bf4118e
coolluck7 <coolluck7@gmail.com>
2019-07-06 14:10:23 +0300

    Add press SearchButton

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,16 +1,24 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
 
 
 public class MainClass {
 
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "D:\\Java\\testselenium\\drivers\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "D:\\Java\\drivers\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
 
-        driver.get("https://ain.ua/");
+        driver.get("https://en.wikipedia.org/");
+
+        driver.findElement(By.xpath("//input[@id='searchButton']")).click();
+
+//        driver.quit();
 
     }
 }
