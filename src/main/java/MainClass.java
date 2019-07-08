6792c9ffe966c31e4e330e689feab67c0a41520f
coolluck7 <coolluck7@gmail.com>
2019-07-08 07:40:06 +0300

    Wikipedia log in and log out

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -11,16 +11,21 @@ public class MainClass {
         System.setProperty("webdriver.chrome.driver", "D:\\Java\\drivers\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+
+        String userName = "TestUserTest123";
+        String password = "123TestUserTest";
+        String email = "b3713847@urhen.com";
 
         driver.get("https://en.wikipedia.org/");
-        driver.findElement(By.xpath("//input[@name='search']")).sendKeys("Selenium WebDriver");
-        driver.findElement(By.xpath("//input[@id='searchButton']")).click();
-        System.out.println(driver.findElement(By.xpath("(//input[@name='search'])[1]")).getAttribute("value"));
-        driver.findElement(By.xpath("(//input[@name='search'])[1]")).clear();
-
-//        driver.quit();
+        driver.findElement(By.xpath("//*[contains(text(), 'Log in')]")).click();
+        driver.findElement(By.xpath("//input[@id='wpName1']")).sendKeys(userName);
+        driver.findElement(By.xpath("//input[@id='wpPassword1']")).sendKeys(password);
+        driver.findElement(By.xpath("//button[@id='wpLoginAttempt']")).click();
+        driver.findElement(By.xpath("//a[contains(text(), 'Log out')]")).click();
+        driver.quit();
 
     }
 }
